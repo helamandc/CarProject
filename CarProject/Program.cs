128f32377764bd122d7e74e00266a6ecae87b4cf
Helaman Del Castillo <helaman.delcastillo@praemium.com>
2023-02-24 00:24:13 +1100

    Added source of project

--- CarProject/Program.cs
+++ CarProject/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace CarProject
 {
+    //https://www.exercisescsharp.com/oop/interfaces
     internal class Program
     {
         static void Main(string[] args)
