2842b30975b9357a49e9b7436a7fec3459cc6072
Helaman Del Castillo <helaman.delcastillo@praemium.com>
2023-02-23 23:51:36 +1100

    Add project files.

---
+++ CarProject/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The car needs fuel to start driving. Please enter fuel(litres): ");
+            int gas = int.Parse(Console.ReadLine());
+
+            Car myCar = new Car();
+            
+            myCar.Refuel(gas);
+            myCar.Drive();
+
+        }
+    }
+
+    public class Car
+    {
+        public void Drive()
+        {
+            if (Refuel == true)
+                Console.WriteLine("Driving");
+        }
+
+        public bool Refuel(int amtGas)
+        {
+            if(amtGas > 0)
+            return true;
+        }
+    }
+
+}
